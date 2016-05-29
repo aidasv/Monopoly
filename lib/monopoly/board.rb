bd1e6f3750c3c38be31a1d3c15412a6740ab4403
Aidas <Aidas>
2016-05-29 15:50:36 +0300

    adding htm generator

--- lib/monopoly/board.rb
+++ lib/monopoly/board.rb
@@ -1,5 +1,11 @@
+require 'erb'
+
 class Board
   def draw
+    template = File.read('index.html.erb')
+    render = ERB.new(template)
+    File.open('index.html', "w").write(render.result)
+
   end
 
 end
